0bfd9450c16972674b9c87e3d854de1376aec266
João Souza <47926930+JoaoVictorFernandesSouza@users.noreply.github.com>
2023-03-21 15:39:10 -0300

    Adiciona projeto casa aberta de 2020

---
+++ js_dicromacia.js
@@ -0,0 +1,29 @@
+function alter_ImagemNormal() {
+    document.getElementById("image1").src = "imagens/protanopia_normal.jpg";
+    document.getElementById("image2").src = "imagens/deutoranopia_normal.jpg";
+    document.getElementById("image3").src = "imagens/tritanopia_normal.jpg";
+}
+
+function alter_ImagemProtanopia() {
+    document.getElementById("image1").src = "imagens/p1.png";
+    document.getElementById("image2").src = "imagens/d1.png";
+    document.getElementById("image3").src = "imagens/t1.png";
+}
+
+function alter_ImagemDeuteranopia() {
+    document.getElementById("image1").src = "imagens/p2.png";
+    document.getElementById("image2").src = "imagens/d2.png";
+    document.getElementById("image3").src = "imagens/t2.png";
+}
+
+function alter_ImagemTritanopia() {
+    document.getElementById("image1").src = "imagens/p3.png";
+    document.getElementById("image2").src = "imagens/d3.png";
+    document.getElementById("image3").src = "imagens/t3.png";
+}
+
+function alter_ImagemMonocromia() {
+    document.getElementById("image1").src = "imagens/p4.png";
+    document.getElementById("image2").src = "imagens/d4.png";
+    document.getElementById("image3").src = "imagens/t4.png";
+}
